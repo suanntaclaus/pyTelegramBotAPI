b9e1c319dedd0ed56cd63b877660d7f6e34ad121
suanntaclaus <blemishedperfection@hotmail.com>
2018-06-02 02:26:30 +0800

    'help'

--- telebot.py
+++ telebot.py
@@ -4,27 +4,27 @@ import datetime
 
 class BotHandler:
 
-def __init__(self, token):
+	def __init__(self, token):
     	self.token = 616484809:AAFyFLE0JHrxHOEsHXaa-gvGXYk7Tg19HxQ
     	self.api_url = 
-"https://api.telegram.org/bot{}/".format(token)
-def get_updates(self, offset=None, timeout=30):
+	"https://api.telegram.org/bot{}/".format(token)
+	def get_updates(self, offset=None, timeout=30):
     	method = 'getUpdates'
     	params = {'timeout': timeout, 'offset': offset}
     	resp = requests.get(self.api_url + method, params)
     	result_json = resp.json()['result']
     	return result_json
-def send_message(self, chat_id, text):
+	def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
         return resp
-def get_last_update(self):
+	def get_last_update(self):
         get_result = self.get_updates()
-if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
+	if len(get_result) > 0:
+            	last_update = get_result[-1]
+        	else:
+            	last_update = get_result[len(get_result)]
 return last_update
 
 greet_bot = BotHandler(token)  
