0d4221019206542c5886a9ae1db244c19870ffb8
suanntaclaus <blemishedperfection@hotmail.com>
2018-06-02 03:32:54 +0800

    'gg'

--- telebot.py
+++ telebot.py
@@ -1,38 +1,78 @@
-import requests 
-import datetime
+# -*- coding: utf-8 -*-
+# This bot was made specifically for the pyTelegramAPI Telegram chat,
+# and goes by the name 'TeleBot (@pyTeleBot)'. Join our group to talk to him!
+# WARNING: Tested with Python 2.7
 
-url = "https://api.telegram.org/bot<616484809:AAFyFLE0JHrxHOEsHXaa-gvGXYk7Tg19HxQ>/"
+import telebot
+import os
 
-def get_updates_json(request):  
-    params = {'timeout': 100, 'offset': None}
-    response = requests.get(request + 'getUpdates', data=params)
-    return response.json()
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
 
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
 
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+}
 
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+if "TELEBOT_BOT_TOKEN" not in os.environ or "GROUP_CHAT_ID" not in os.environ:
+    raise AssertionError("Please configure TELEBOT_BOT_TOKEN and GROUP_CHAT_ID as environment variables")
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
+bot = telebot.AsyncTeleBot(os.environ["616484809:AAFyFLE0JHrxHOEsHXaa-gvGXYk7Tg19HxQ"])
+GROUP_CHAT_ID = int(os.environ["GROUP_CHAT_ID"])
 
-send_mess(chat_id, 'Your message goes here')
+def is_api_group(chat_id):
+    return chat_id== GROUP_CHAT_ID
 
-def main():  
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)       
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return
 
-if __name__ == '__main__':  
-    main()
+    name = message.new_chat_participant.first_name
+    if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
+        name += u" {}".format(message.new_chat_participant.last_name)
+
+    if hasattr(message.new_chat_participant, 'username') and message.new_chat_participant.username is not None:
+        name += u" (@{})".format(message.new_chat_participant.username)
+
+    bot.reply_to(message, text_messages['welcome'].format(name=name))
+
+@bot.message_handler(commands=['info', 'help'])
+def on_info(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+    bot.reply_to(message, text_messages['info'])
+
+@bot.message_handler(commands=["ping"])
+def on_ping(message):
+    bot.reply_to(message, "Still alive and kicking!")
+
+@bot.message_handler(commands=['start'])
+def on_start(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+def listener(messages):
+    for m in messages:
+        print str(m)
+
+bot.set_update_listener(listener)
+bot.polling()
+
+
