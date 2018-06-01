cf0f4bf642247ce57cd273f9f52805ee0d06462c
suanntaclaus <blemishedperfection@hotmail.com>
2018-06-02 02:33:00 +0800

    'yikes'

--- telebot.py
+++ telebot.py
@@ -5,16 +5,16 @@ import datetime
 class BotHandler:
 
 	def __init__(self, token):
-    	self.token = 
+        self.token = 
     "616484809:AAFyFLE0JHrxHOEsHXaa-gvGXYk7Tg19HxQ"
-    	self.api_url = 
+        self.api_url = 
 	"https://api.telegram.org/bot{}/".format(token)
 	def get_updates(self, offset=None, timeout=30):
-    	method = 'getUpdates'
-    	params = {'timeout': timeout, 'offset': offset}
-    	resp = requests.get(self.api_url + method, params)
-    	result_json = resp.json()['result']
-    	return result_json
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 	def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
@@ -23,9 +23,9 @@ class BotHandler:
 	def get_last_update(self):
         get_result = self.get_updates()
 	if len(get_result) > 0:
-            	last_update = get_result[-1]
-        	else:
-            	last_update = get_result[len(get_result)]
+                last_update = get_result[-1]
+            else:
+                last_update = get_result[len(get_result)]
 return last_update
 
 greet_bot = BotHandler(token)  
