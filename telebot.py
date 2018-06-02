bfd27dd4699a59a5d1d0f24a9d20ab6547fb5537
suanntaclaus <blemishedperfection@hotmail.com>
2018-06-02 17:51:16 +0800

    'hh'

--- telebot.py
+++ telebot.py
@@ -6,70 +6,33 @@
 #{"ok":true,"result":[{"update_id":959942309,
 #"message":{"message_id":15,"from":{"id":236704084,"is_bot":false,"first_name":"Su-ann","username":"suanntaclaus","language_code":"en-GB"},"chat":{"id":236704084,"first_name":"Su-ann","username":"suanntaclaus","type":"private"},"date":1527910904,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}},{"update_id":959942310,
 #"message":{"message_id":16,"from":{"id":236704084,"is_bot":false,"first_name":"Su-ann","username":"suanntaclaus","language_code":"en-GB"},"chat":{"id":236704084,"first_name":"Su-ann","username":"suanntaclaus","type":"private"},"date":1527910905,"text":"hello"}}]}
-import urllib
-import json 
-import requests
-import time
+# - *- coding: utf- 8 - *-
+# - *- coding: utf- 8 - *-
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = "616484809:AAFyFLE0JHrxHOEsHXaa-gvGXYk7Tg19HxQ"
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-
-
-def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
-
-
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
-
-def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
-    if offset:
-        url += "&offset={}".format(offset)
-    js = get_json_from_url(url)
-    return js
-
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
-
-def get_last_chat_id_and_text(updates):
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
-
-def echo_all(updates):
-    for update in updates["result"]:
-        try:
-            text = update["message"]["message"]
-            chat = update["message"]["chat"]["id"]
-            send_message(text, chat)
-        except Exception as e:
-            print(e)
-
-def send_message(text, chat_id):
-    text = urllib.parse.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url)
-    
+def start(bot, update):
+  update.message.reply_text("I'm a bot, Nice to meet you!")
+  
+def convert_uppercase(bot, update):
+  update.message.reply_text(update.message.text.upper())
 
 def main():
-    last_update_id = None
-    while True:
-        updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
-        time.sleep(0.5)
+  # Create Updater object and attach dispatcher to it
+  updater = Updater(TOKEN)
+  dispatcher = updater.dispatcher
+  print("Bot started")
 
+  # Add command handler to dispatcher
+  start_handler = CommandHandler('start',start)
+  upper_case = MessageHandler(Filters.text, convert_uppercase)
+  dispatcher.add_handler(start_handler)
+  dispatcher.add_handler(upper_case)
+
+  # Start the bot
+  updater.start_polling()
+
+  # Run the bot until you press Ctrl-C
+  updater.idle()
 
 if __name__ == '__main__':
-    main()
+  main()
